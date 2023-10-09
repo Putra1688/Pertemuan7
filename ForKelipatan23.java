fd48a64b8ff7d2b8f36743e30c01b33a8cac8b6c
Putra1688 <ranggadsaputra11@gmail.com>
2023-10-09 13:35:56 +0700

    new file

---
+++ ForKelipatan23.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class ForKelipatan23 {
+        public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int kelipatan, jumlah=0, counter=0;
+
+        System.out.println("Masukkan bilangan kelipatan (1-9) : ");
+        kelipatan = scan.nextInt();
+
+    //  for (inisialisasi; kondisi; update) { statement1; statement2;}
+        for ( int i = 1; i <=50; i++) {
+            if (i % kelipatan == 0) {
+                jumlah += i; //menghitung akumulasi dari banyaknya nilai
+                counter++; //menghitung banyaknya nilai
+            }
+        }
+    System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d", kelipatan, counter);
+    System.out.printf("\nTotal bilangan kelipatan %d dari 1 sampai 50 adalah %d", kelipatan, jumlah);
+    }
+}
---
+++ WhileGaji23.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class WhileGaji23 {
+    public static void main(String[] args) {
+
+    int jumlahKaryawan, jumlahJamLembur, gajiLembur
+    }
+}
