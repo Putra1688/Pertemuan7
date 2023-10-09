0ca72f47c6117e57aa4c7aaac2089c5923667e8b
Putra1688 <ranggadsaputra11@gmail.com>
2023-10-09 16:34:43 +0700

    Do-While

---
+++ DoWhileCuti23.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class DoWhileCuti23 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int jatahCuti, jumlahHari;
+    String konfirmasi;
+
+    System.out.println("Jatah Cuti: ");
+    jatahCuti = sc.nextInt();
+    
+    do { 
+        System.out.println("Apakah anda ingin mengambil cuti? (y/n)");
+        konfirmasi = sc.next();
+
+        if (konfirmasi.equalsIgnoreCase("y")) {
+            System.out.println("Jumlah Hari: ");
+            jumlahHari = sc.nextInt();
+
+                if (jumlahHari < jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " +jatahCuti);
+                } else  {
+                    System.out.println("Sisa jatah cuti anda tidak mencukupi");
+                    break;
+                }
+        }
+    } while (jatahCuti > 0);
+    }
+}
